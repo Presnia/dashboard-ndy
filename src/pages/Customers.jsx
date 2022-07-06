534aa20473830c708de408ad1bf4954f3df8f957
Andrei Dzebysh <presnia@yahoo.com>
2022-07-06 13:06:59 +0300

    The Customers and the Employees components are corrected a bit

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -20,6 +20,7 @@ const Customers = () => {
         <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
             <Header title="Customers" category="Page" />
             <GridComponent
+                id="gridcomp"
                 dataSource={customersData}
                 allowPaging
                 allowSorting
